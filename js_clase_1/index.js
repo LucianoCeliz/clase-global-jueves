9d35020553a27a8270bf786780c9e65e506e23da
LucianoCeliz <celizluciano21@gmail.com>
2025-02-24 19:57:38 -0300

    clases

---
+++ js_clase_1/index.js
@@ -0,0 +1,56 @@
+
+
+/* console.log() : Por medio de este comando voy a hacer conextion entre la hoja de java js y la consola del navegador */
+                   //Depuracion
+/* console.log(32)
+
+console.log("me gusta el queso") */
+
+// JavaScript es un lenguaje de programación.
+// JS originalmente se creo para la WEB. Y solo para la web.
+// Cambio en el 2008 con la salida de Node.js
+// Node.js es un entorno para ejecutar JS del lado del servidor.
+// Nacio en el año 1995 -> Creador : Brendan Eich
+// Cuando se creo el lenguaje mas popular del mercado era Java .
+/* 
+    ¿Que podemos hacer con JS:
+    - Desarrollo web
+    - Desarrollo Mobile
+    - App de escritorio
+    - Inteligencia Articial
+    - Machine Learning
+    - Desarollo de videojuegos
+    - Modelaje 3D
+    - Programar y controlar hardware
+    - Robotica 
+*/
+
+
+//Otros lenguajes:
+       /* Java
+          Phyton
+          PHP
+          C#
+          C++
+          C
+          Ruby
+          Go
+          Rust
+          Cobol*/
+
+// Lenguajes utilizados mayormente para realizar:
+                  // Inteligencia artificial o Machine Learning > Python
+                  // Sitios Web, aplicaciones moviles o videojuegos para la web -> Java Scrips -> Phaser
+                  // Videojuegos de tipo INDIE (2D) -> C# -> Unity        
+                  // Videosjuegos de gran escala -> C++ -> Unreal Engine
+                  // Contratos de blockchain -> Solidity
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Los lenguajes de programacion utilizan tipos de datos.
+// Cada tipo de dato tienen sus propias caracteristicas.
+// Vamos a ver los primeros 3:
+// 1. Numericos (number) ->  Numeros hechos y derechos (naturales, enteros, etc)
+// 2. Cadenas de texto (string) -> Caracteres encadenados 
+// 3. Booleanos (booleans) -> Lo que proviene de algebra de boole. Que nos da falsedad o verdad
